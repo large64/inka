4f715bb80d4655e6e0dcceca814479f9bc2973d1
ballaibrigitt42 <ballaibrigitta42@gmail.com>
2015-04-29 18:26:35 +0200

    Change harder and easy, add extra methods

--- src/Card/CardDeck.java
+++ src/Card/CardDeck.java
@@ -21,9 +21,9 @@ public class CardDeck {
         easyCards = new HashMap();
         hardCards = new HashMap();
         harderCards = new HashMap();
-        easyCardNumber = 6;
+        easyCardNumber = 1;
         hardCardNumber = 2;
-        harderCardNumber = 1;
+        harderCardNumber = 6;
         studying = false;
     }
     
@@ -48,39 +48,17 @@ public class CardDeck {
         studying = false;
     }
     
-    /*public void askingWordsFromUser(){
-        int i = easyCardNumber;
-        int j = hardCardNumber;
-        int k = harderCardNumber;
-        
-        while(studying){
-            while(!easyCards.isEmpty() && i > 0){
-            // ask from easyword
-                askingFromEasyWords();
-                --i;
-            }
-            while(!hardCards.isEmpty() && j > 0){
-                // ask from hardcards
-                --j;
-            }
-            while(!harderCards.isEmpty() && k > 0){
-                // ask from harderCards
-                --k;
-            }
-            
-            // if all of the maps are empty then the studying is finished automatically
-            if(easyCards.isEmpty() && hardCards.isEmpty() && harderCards.isEmpty()){
-                studying = false;
-                break;
-            }
-        }
+    public HashMap<String, String> getEasyCards(){
+        return easyCards;
     }
     
-    private void askingFromEasyWords(){
-        for(String key : easyCards.keySet()){
-            
-        }
-    }*/
+    public HashMap<String, String> getHardCards(){
+        return hardCards;
+    }
+    
+    public HashMap<String, String> getHarderCards(){
+        return harderCards;
+    }
     
     // delete this function
     public void WriteOutEverything(){
@@ -93,12 +71,12 @@ public class CardDeck {
     
     // put cards into easy cards
     // if there is no enough card the user has to add more cards 
-    public void loadEasyCards(){
-        if(cards.size() >= easyCardNumber){
+    public void loadHarderCards(){
+        if(cards.size() >= harderCardNumber){
             // if the size is the same with the number put all the cards into the easycards
-            if(cards.size() == easyCardNumber){
-                easyCards.putAll(cards);
-                WriteOutEasyCards();
+            if(cards.size() == harderCardNumber){
+                harderCards.putAll(cards);
+                WriteHarderCards();
             }else{
                 // String array is just help to choose cards
                 String[] words = new String[cards.size()];
@@ -107,41 +85,41 @@ public class CardDeck {
                     words[i] = key;
                     ++i;
                 }
-                String[] readyWords = new String[easyCardNumber];
+                String[] readyWords = new String[harderCardNumber];
                 readyWords = ChooseRandomWords(words);
                 // now i have the different words int readyWords and I put them into the easyCards
                 for(int k = 0; k < readyWords.length; ++k){
                     String english = readyWords[k];
                     String hungarian = cards.get(readyWords[k]);
-                    easyCards.put(english, hungarian);
+                    harderCards.put(english, hungarian);
                 }
                 // Delete this function!
-                WriteOutEasyCards();
+                WriteHarderCards();
             }
         // if there is not enough card, tell user to upload more cards
         }else{
-            System.out.println("Sorry, you do not have enough card, please put at least " + easyCardNumber + " cards");
+            System.out.println("Sorry, you do not have enough card, please put at least " + harderCardNumber + " cards");
         }
     }
     
     // Delete this function!
-    private void WriteOutEasyCards()
+    private void WriteHarderCards()
     {
-        System.out.println("EasyCards");
-        for(String key : easyCards.keySet()){
-            System.out.println("key : " + key + "\tvalue : " + easyCards.get(key));
+        System.out.println("HarderCards");
+        for(String key : harderCards.keySet()){
+            System.out.println("key : " + key + "\tvalue : " + harderCards.get(key));
         }
     }
     
     // Choose random words, this is just a helper function
     private String[] ChooseRandomWords(String[] words){
-        String[] choosenWords = new String[easyCardNumber];
+        String[] choosenWords = new String[harderCardNumber];
         
         Random rnd = new Random();
         int myRandomNumber;
-        int[] generatedRandomNumbers = new int[easyCardNumber];
+        int[] generatedRandomNumbers = new int[harderCardNumber];
         int j = 0;
-        while(j < easyCardNumber){
+        while(j < harderCardNumber){
             myRandomNumber = rnd.nextInt(words.length);
             if(ThereIsNotInIntArray(myRandomNumber, generatedRandomNumbers)){
                 choosenWords[j] = words[myRandomNumber];
@@ -177,7 +155,7 @@ public class CardDeck {
     // set the numbers of the cards which mush have been asked
     // in the consturctor these will be auttomatically set for the user
     // but the user has the chance to set it another numbers
-    public void setEaseCardNumber(int number){
+    public void setEasyCardNumber(int number){
         easyCardNumber = number;
     }
     
@@ -187,5 +165,35 @@ public class CardDeck {
     
     public void setHarderCardNumber(int number){
         harderCardNumber = number;
+    }
+    
+    public int getEasyCardNumber(){
+        return easyCardNumber;
+    }
+    
+    public int getHardCarNumber(){
+        return hardCardNumber;
+    }
+    
+    public int getHarderCardNumber(){
+        return harderCardNumber;
+    }
+    
+    // delete from hard, harder, easy
+    public void removeCardFromHarderCards(String english){
+        harderCards.remove(english);
+    }
+    
+    public void removeCardFromHardCards(String english){
+        hardCards.remove(english);
+    }
+    
+    public void removeCardFromEasyCards(String english){
+        easyCards.remove(english);
+    }
+    
+    // add to hard, harder, easy
+    public void addCardToHarderCards(String englihs, String hungarian){
+        harderCards.put(englihs, hungarian);
     }
 }
