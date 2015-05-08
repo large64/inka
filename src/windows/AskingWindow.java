0388735da0633bcb10ece94c9162f19b10b1f941
memphis17 <memphis817@gmail.com>
2015-05-08 18:40:49 +0200

    Set windows title

--- src/windows/AskingWindow.java
+++ src/windows/AskingWindow.java
@@ -421,19 +421,19 @@ public class AskingWindow extends JPanel{
             switch (buttonText) {
                 case "Menu":
                     Inka.getWindow().changePanel(0);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
                 case "Ask":
                     Inka.getWindow().changePanel(1);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
                 case "Grammar":
                     Inka.getWindow().changePanel(2);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
                 case "Card manager":
                     Inka.getWindow().changePanel(3);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
             }
         }
--- src/windows/MainWindow.java
+++ src/windows/MainWindow.java
@@ -73,19 +73,19 @@ public class MainWindow extends JFrame {
             switch (buttonText) {
                 case "Menu":
                     Inka.getWindow().changePanel(0);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
                 case "Ask":
                     Inka.getWindow().changePanel(1);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
                 case "Grammar":
                     Inka.getWindow().changePanel(2);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
-                case "Card Manager":
+                case "Card manager":
                     Inka.getWindow().changePanel(3);
-                    Inka.getWindow().setTitle(buttonText);
+                    Inka.getWindow().setTitle("Inka - " + buttonText);
                     break;
             }
         }
